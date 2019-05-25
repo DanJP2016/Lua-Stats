74dd0ebfcc0880c686266a6d28f6df0b56ad5ba0
Daniel <danpirtle1@gmail.com>
2019-05-24 22:08:28 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Statistics.lua
+
+Small statistical library for working with numerical data in Lua.
+
+Disclaimer: I am not a math person. Corrections and recommendations greatly appreciated.
+
+
+## Usage
+
+To use this library 
+
+    - clone this repo into the folder or your project
+    - use 'require' to load the library into your projects files
+
+    example: 
+    local stats = require('statistics')
---
+++ statistics.lua
@@ -0,0 +1,110 @@
+statistics = {}
+
+function statistics:mean(data)
+  assert(type(data) == 'table', 'MODE function PARAMETER: DATA must be a TABLE')
+  local sum = 0
+
+  for i = 1, #data do
+    sum = sum + data[i]
+  end
+
+  return sum
+end
+
+function statistics:median(data)
+  assert(type(data) == 'table', 'MEDIAN function PARAMETER: DATA must be a TABLE')
+  table.sort(data)
+
+  local m = 0
+
+  if #data % 2 == 0 then
+    local a = data[#data / 2]
+    local b = data[(#data / 2) + 1]
+
+    m = (a + b) / 2
+  else
+    m = data[math.ceil(#data / 2)]
+  end
+
+  return m
+end
+
+function statistics:range(data)
+  assert(type(data) == 'table', 'RANGE function PARAMETER: DATA must be a TABLE')
+
+  table.sort(data)
+  return data[#data] - data[1]
+end
+
+function statistics:variance(data, m)
+  assert(type(data) == 'table', 'VARIANCE function PARAMETER: DATA must be a TABLE')
+
+  m = m or statistics:mean(data)
+  assert(type(m) == 'number', 'VARIANCE function PARAMETER: M must be a NUMBER')
+
+  local dif = 0
+
+  for i = 1, #data do
+    dif = dif + ((data[i] - m) ^ 2)
+  end
+
+  return dif / #data
+end
+
+
+-- sample variance
+function statistics:s_variance(data, m)
+  assert(type(data) == 'table', 'S_VARIANCE function PARAMETER: DATA must be a TABLE')
+
+  m = m or statistics:mean(data)
+  assert(type(m) == 'number', 'S_VARIANCE function PARAMETER: M must be a NUMBER')
+
+  local dif = 0
+
+  for i = 1, #data do
+    dif = dif + ((data[i] - m) ^ 2)
+  end
+
+  return dif / (#data - 1)
+end
+
+-- standard deviation
+function statistics:sd(v)
+  assert(type(v) == 'number', 'SD function PARAMETER: V must be a NUMBER')
+  return math.sqrt(v)
+end
+
+function statistics:z_score(val, m, sd)
+  assert(type(data) == 'table', 'Z_SCORE function PARAMETER: DATA must be a TABLE')
+  assert(type(m) == 'number', 'Z_SCORE function PARAMETER: M must be a NUMBER')
+  assert(type(v) == 'number', 'Z_SCORE function PARAMETER: SD must be a NUMBER')
+  return (val - m) / sd
+end
+
+-- return a table with key:value pairs, option to show the output in the terminal
+function statistics:z_score_all(data, m, sd, show)
+  assert(type(data) == 'table', 'Z_SCORE_ALL function PARAMETER: DATA must be a TABLE')
+  assert(type(m) == 'number', 'Z_SCORE_ALL function PARAMETER: M must be a NUMBER')
+  assert(type(v) == 'number', 'Z_SCORE_ALL function PARAMETER: SD must be a NUMBER')
+
+  show = show or false
+  assert(type(show) == 'boolean', 'SHOW must be a BOOLEAN value')
+
+  local scores = {}
+
+  for i = 1, #data do
+    scores[data[i]] = (data[i] - m) / sd
+  end
+
+  if show == true then
+    print('value', 'score')
+
+    for i = 1, #data do
+      print(data[i], (data[i] - m) / sd)
+    end
+  end
+
+  return scores
+end
+
+return statistics
